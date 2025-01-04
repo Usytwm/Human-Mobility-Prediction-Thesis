e61613cc1d2eb91e5542a87e9c2803d571050c21
brian <brian@jobs2live.com>
2025-01-04 17:51:24 -0500

    agrega funciones para cargar datos de movilidad desde diferentes archivos CSV y crea un cuaderno para guardar y analizar los datos

---
+++ utils/common.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+import pandas as pd
+
+from utils.mobility_data_manager import DataManager
+
+
+class Data(Enum):
+    ALL = 1
+    SMALL = 2
+    LAST_3000 = 3
+    LAST_1000 = 4
+
+
+def load_data(type: Data):
+    match type:
+        case Data.ALL:
+            csv_file = "../Data/cityA_groundtruthdata.csv/cityA_groundtruthdata.csv"
+            manager = DataManager(
+                csv_path=csv_file,
+            )
+            df = manager.load_csv()
+        case Data.SMALL:
+            df = pd.read_csv(
+                "../Data/cityA_groundtruthdata.csv/cityA_groundtruthdata_small.csv"
+            )
+        case Data.LAST_1000:
+            df = pd.read_csv(
+                "../Data/cityA_groundtruthdata.csv/cityA_groundtruthdata_last_1000_users.csv"
+            )
+        case Data.LAST_3000:
+            df = pd.read_csv(
+                "../Data/cityA_groundtruthdata.csv/cityA_groundtruthdata_last_3000_users.csv"
+            )
+        case _:
+            df = pd.read_csv(
+                "../Data/cityA_groundtruthdata.csv/cityA_groundtruthdata_small.csv"
+            )
+    return df
